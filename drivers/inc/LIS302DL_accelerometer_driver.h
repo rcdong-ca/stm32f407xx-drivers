68344a6750cc0c2d3d637643c31caea40d549658
rcdong-ca <richard_dong50@hotmail.com>
2024-03-21 12:32:02 -0700

    accelerometer dirver init commit

---
+++ drivers/inc/LIS302DL_accelerometer_driver.h
@@ -0,0 +1,19 @@
+/*
+ * LIS302DL_accelerometer_driver.h
+ *
+ *  Created on: Mar 20, 2024
+ *      Author: richard
+ *
+ *
+ *      Holy this took a long time to find.
+ *      Data sheet of the accelerometer can be found here
+ *      https://www.st.com/resource/en/application_note/an2335-lis302dl-3axis--2g8g-digital-output-ultracompact-linear-accelerometer-stmicroelectronics.pdf
+ */
+
+#ifndef INC_LIS302DL_ACCELEROMETER_DRIVER_H_
+#define INC_LIS302DL_ACCELEROMETER_DRIVER_H_
+
+#define
+
+
+#endif /* INC_LIS302DL_ACCELEROMETER_DRIVER_H_ */
